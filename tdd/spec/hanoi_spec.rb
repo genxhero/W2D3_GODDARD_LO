02c66d9152de52d4d1ae07f974b25ac10ae42056
AaronGoddard <aaronbear@gmail.com>
2018-08-29 15:25:58 -0700

    start towers of hanoi

--- tdd/spec/hanoi_spec.rb
+++ tdd/spec/hanoi_spec.rb
@@ -2,14 +2,33 @@ require "hanoi.rb"
 
 RSpec.describe Game do
   describe "#initialize" do
-    it "begins with a two dimen"
+    it "begins with a two dimensional array with a length of three" do
+    end
+
+    it "first element of matrix contains values 1 to 3 in ascending order" do
+    end
+
   end
 
-  describe "#move"  do
+  describe "#move" do
+    it "prompts the user using gets" do
+    end
 
+    it "checks to see if a move is valid" do
+    end
+
+    it "loops until game is won" do
+
+    end
   end
 
   describe "#won?" do
+    it "returns true if either the second or third tower is full" do
 
+    end
+  end
+
+  describe "valid?" do
+    it ""
   end
 end
